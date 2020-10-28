d902c43b44d8971ba7a6bcb630f61566d93e641a
Brandon Green <Brandon.Green@knights.ucf.edu>
2020-10-28 19:25:57 -0400

    got api/workouts working

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,21 @@
-const router = require("express").Router;
-const Workout = require("../models/workout")
+const router = require("express").Router();
+const Workout = require("../models/workout");
+
 
 router.post("/api/workouts/", (req, res) => {
-    var workout = req.body;
-    console.log(req.body);
+  var workout = req.body;
+  console.log(req.body);
 
-    Workout.create({
-        day: workout.day,
-        exercises: workout.exercises
-    });
+  Workout.create({
+    day: workout.day,
+    exercises: workout.exercises,
+  });
 
-    res.status(204).end();
-})
+  res.json(workout)
+});
+
+// router.put("/api/workouts/:id", (req, res) => {
+//   var id = req.body.id;
+// });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ mongoose.connect("mongodb://localhost/workout", {
 });
 
 // routes
-// app.use(require("./Develop/routes/api"));
+app.use(require("./routes/api"));
 app.use(require("./routes/view"));
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
