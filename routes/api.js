745f808a7a7828bc03d55db6e0fea975a642791e
Brandon Green <Brandon.Green@knights.ucf.edu>
2020-10-30 15:53:20 -0400

    added rest of api routes

--- routes/api.js
+++ routes/api.js
@@ -27,18 +27,40 @@ router.post("/api/workouts/", (req, res) => {
     });
 });
 
-router.put("/api/workouts/:id", ({body, params}, res) => {
+router.put("/api/workouts/:id", ({ body, params }, res) => {
   Workout.findByIdAndUpdate(
     params.id,
-    {$push: {exercises: body}},
+    { $push: { exercises: body } },
     // "runValidators" will ensure new exercises meet our schema requirments
     { new: true, runValidators: true }
-  ).then(dbWorkout => {
-    res.json(dbWorkout);
-  })
-  .catch(err => {
-    res.json(err)
-  })
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .limit(7)
+    .then((dbWorkouts) => {
+      console.log(dbWorkouts);
+      res.json(dbWorkouts);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+router.delete("/api/workouts", ({ body }, res) => {
+  Workout.findByIdAndDelete(body.id)
+    .then(() => {
+      res.json(true);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 module.exports = router;
